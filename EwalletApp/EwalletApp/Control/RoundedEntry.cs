ed1f2fa1cb80a2b5f7982e670210b6159c02a674
prasert <sornprasit.pr@hotmail.com>
2019-08-31 22:09:50 +0700

    Custom button

---
+++ EwalletApp/EwalletApp/Control/RoundedEntry.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace EwalletApp.Control
+{
+   public class RoundedEntry : Entry
+    {
+    }
+}
