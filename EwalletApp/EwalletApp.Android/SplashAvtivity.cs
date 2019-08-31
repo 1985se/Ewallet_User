5024dcd8b3f613bf09a458d21f054b45e05b7dd8
prasert <sornprasit.pr@hotmail.com>
2019-08-31 21:06:58 +0700

    splashpage

--- EwalletApp/EwalletApp.Android/SplashAvtivity.cs
+++ EwalletApp/EwalletApp.Android/SplashAvtivity.cs
@@ -2,17 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using System.Threading.Tasks;
 using Android.App;
 using Android.Content;
 using Android.OS;
 using Android.Runtime;
+using Android.Support.V7.App;
 using Android.Views;
 using Android.Widget;
 
 namespace EwalletApp.Droid
 {
-    class SplashAvtivity
+    [Activity(Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashAvtivity : AppCompatActivity
     {
+        
+
+        public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
+        {
+            base.OnCreate(savedInstanceState, persistentState);
+            
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Task startupWork = new Task(() => { SimulateStartup(); });
+            startupWork.Start();
+        }
+        async void SimulateStartup()
+        {
+            
+            await Task.Delay(2000); // Simulate a bit of startup work.            
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+
     }
 }
